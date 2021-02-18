9ff3556fa6318b01e41685f72a78a03829875e8b
Jerome-LorD <jerome-divry@laposte.net>
2021-02-18 06:01:05 +0100

    refactoring

--- app/controllers/select_substitute.py
+++ app/controllers/select_substitute.py
@@ -1,65 +1,84 @@
 #!/usr/bin/env python
 """Select detail product module."""
-from app.settings import QUIT_APP, MSG_SAVE, MSG_ERROR
-
+from app import settings as s
 from app.models.product import Product
 from app.views.main_page import View
 
 
 class SelectSubstitute:
-    """Select substitute."""
+    """Select substitute.
+
+    Get the product instance and then, whith the product id, the substitute instance
+    is found.
+    """
 
     def __init__(self, instance):
         """Init."""
         self.product: Product = Product()
         self.view: View = View()
-        self.instance = instance[0]
+        self.prdt_instance = instance[0]
         self.substitute = self.product.find_substitute(
-            selected_product=self.instance.pk
+            selected_product=self.prdt_instance.pk
         )
-        self.indexes = [str(index) for index in range(1, len(MSG_SAVE) + 1)]
-        self.possible_commands = ["back-to-menu", QUIT_APP]  # settings
+        self.indexes = [str(index) for index in range(1, len(s.MSG_SAVE) + 1)]
+        self.possible_commands = ["save", "select-menu-2", "back-to-menu", s.QUIT_APP]
 
     def display(self):
         """Display the substitute."""
-        if self.instance.nutriscore == "a":
+        if self.prdt_instance.nutriscore == "a":
             self.view.display_no_better_nutriscore()
         elif (
-            self.instance.nutriscore != "a"
-            and self.substitute.nutriscore == self.instance.nutriscore
+            self.prdt_instance.nutriscore != "a"
+            and self.substitute.nutriscore == self.prdt_instance.nutriscore
         ):
             self.view.display_no_subsitute()
         else:
             self.view.display_subsitute(details=self.substitute)
 
     def get_input(self):
-        """Get input."""
-        choice = input(self.view.input_save())
+        """Get input.
+
+        ask for save or find the saved substitutes.
+        """
+        choice = self.view.input_save()
+        choice = input(s.MSG_CHOICE)
         if choice == "m":
             return "back-to-menu"
         if choice == "q":
-            return QUIT_APP
+            return s.QUIT_APP
+        if choice in self.indexes:
+            if choice == "1":
+                return "save"
+            if choice == "2":
+                return "select-menu-2"
         return choice
 
     def update(self, command: str):
-        """Update the controller."""
-        if command in self.indexes:
-            # command = self.categories[int(command) - 1]
-            # if command == "save":
-            if self.substitute:
-                self.product.save_substitute(self.instance.pk, self.substitute.pk)
-                return "save"
-            else:
-                self.product.save_substitute(self.instance.pk, self.instance.pk)
-                return "save"
-            # if command == "menu":
-            #     return "menu"
+        """Update the controller.
 
-        elif command in self.possible_commands:
-            if command == QUIT_APP:
+        Prompt and save the product in db if requested.
+        """
+        if command in self.possible_commands:
+            if command == "select-menu-2":
+                return command
+
+            if command == "save":
+                if self.substitute:
+                    if self.substitute.nutriscore < self.prdt_instance.nutriscore:
+                        self.product.save_substitute(
+                            self.prdt_instance.pk, self.substitute.pk
+                        )
+                        return "save"
+                    else:
+                        self.product.save_substitute(
+                            self.prdt_instance.pk, self.prdt_instance.pk
+                        )
+                        return "save"
+            if command == s.QUIT_APP:
                 self.walk = False
             if command == "back-to-menu":
                 return "back-to-menu"
         else:
-            return MSG_ERROR
+            s.ERROR = True
+            return s.MSG_ERROR
         return command
