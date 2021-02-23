0335db6ebb6922ed1c6f4cca94bd26b5bbbc4952
Jerome-LorD <jerome-divry@laposte.net>
2021-02-23 06:14:44 +0100

    Refactoring

--- app/controllers/select_category.py
+++ app/controllers/select_category.py
@@ -3,7 +3,7 @@
 from app import settings as s
 
 from app.models.category import Category
-from app.views.main_page import View
+from app.views.view import View
 
 
 class SelectCategory:
@@ -14,27 +14,31 @@ class SelectCategory:
         self.view = View()
         self.categories = Category.retrieve()
         self.indexes = [str(index) for index in range(1, len(self.categories) + 1)]
-        self.possible_commands = [s.QUIT_APP]
+        self.possible_commands = [s.BACK_TO_MENU, s.QUIT_APP]
 
     def display(self):
         """Display."""
-        self.view.display_categories(categories=self.categories)
+        self.view.display_categories()
 
     def get_input(self):
         """Get input."""
-        choice = self.view.input_message(self.categories)
+        choice = self.view.input_categories(categories=self.categories)
         choice = input(s.MSG_CHOICE)
+        if choice == "m":
+            return s.BACK_TO_MENU
         if choice == "q":
             return s.QUIT_APP
         return choice
 
-    def update(self, command: str):
+    def update(self, command: str) -> str:
         """Update the controller."""
         if command in self.indexes:
-            command = self.categories[int(command) - 1]
-            return f"select-category-{command.product_id}"
+            category = self.categories[int(command) - 1]
+            return f"select-category-{category.product_id}"
 
         elif command in self.possible_commands:
+            if command == "back-to-menu":
+                return s.BACK_TO_MENU
             if command == s.QUIT_APP:
                 self.walk = False
         else:
