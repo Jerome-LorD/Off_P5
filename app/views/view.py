58f12b4fcd8c8a8ddcaf2a9e3ceef63ca46ec720
Jerome-LorD <jerome-divry@laposte.net>
2021-02-23 06:29:34 +0100

    Refactoring

--- app/views/view.py
+++ app/views/view.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
-"""Main page view."""
+"""View module."""
+import os
+
 from colorama import init
 from termcolor import colored
 
@@ -12,168 +14,222 @@ init(autoreset=True)
 
 
 class View:
-    """Main page."""
+    """View class."""
 
     def __init__(self):
         """Init."""
         self.category: Category = Category()
         self.product: Product = Product()
-        self.commands = ""
+
+    def clear(self):
+        """Clear the terminal."""
+        os.system("cls" if os.name == "nt" else "clear")
+
+    def justify(self, text="", lenght: int = 0):
+        """Justify the text."""
+        return "{:>{}}".format(text, (len(text) + lenght))
 
     def header(self):
         """Display the header."""
+        self.clear()
         return f"\n{s.DASHES}\n{s.HEADER}\n{s.DASHES}"
 
-    def display_categories(self, categories):
+    def display_main_menu(self):
+        """Display the main menu."""
+        print(self.header())
+
+    def display_categories(self):
         """Display the categories."""
         print(self.header())
-        print("\nSelectionnez une catégorie :\n")
-        if s.ERROR:
-            print(s.MSG_ERROR)
+        print(f"\n{s.SELECT_CATEGORY}\n")
 
     def display_products(self, products, max_pages, page):
         """Display the products."""
         print(self.header())
         print(
-            f"\nSélectionnez un produit : page {page} / {max_pages}\n\
-{colored('n', 'yellow')} : Page suivante\n{colored('p', 'yellow')} : Page précédente\n"
-        )
-        if s.ERROR:
-            print(s.MSG_ERROR)
-
-    def display_detail_tst(self, instance):
-        """Display the products details."""
-        print("\nDétail du produit sélectionné :\n")
-        print(
-            f"Produit : {instance.name}\nMarque : {instance.brand}\nMagasin :\
- {instance.stores}\nURL : {instance.url}\nNutriscore :\
- {instance.nutriscore.capitalize()}"
+            f"\n{s.SELECT_PRODUCT} page {page} / {max_pages}\n\n{s.NEXT_PREVIOUS_PAGE}"
         )
 
-    def display_subsitute(self, details):
-        """Display the substitute."""
-        print("\nDétail du produit de substitution :\n")
-        print(
-            f"Produit : {details.name}\nMarque : {details.brand}\nMagasin :\
- {details.stores}\nURL : {details.url}\nNutriscore : \
- {details.nutriscore.capitalize()}\n"
-        )
-        print(s.SUB_HEADER)
+    def display_details(self, substituted, substitute):
+        """Display the saved products details."""
+        print(self.header())
+        print(f"\n{s.SUBSTITUTED_DETAIL} :\n")
+        for item in [substituted]:
+            print(
+                f"{s.PRODUCT} : {item.name}\
+\n{s.BRAND} : {item.brand.replace(',', ', ')}\
+\n{s.STORES} : {item.stores.replace(',', ', ')}\
+\n{s.URL} : {item.url}\
+\n{s.NUTRISCORE.capitalize()} : {item.nutriscore.capitalize()}"
+            )
+        print(f"\n{s.SUBSTITUTE_DETAIL} :\n")
+        for item in [substitute]:
+            print(
+                f"{s.PRODUCT} : {item.name}\
+\n{s.BRAND} : {item.brand.replace(',', ', ')}\
+\n{s.STORES} : {item.stores.replace(',', ', ')}\
+\n{s.URL} : {item.url}\
+\n{s.NUTRISCORE.capitalize()} : {item.nutriscore.capitalize()}"
+            )
+        print(f"{s.CR}{s.SUB_HEADER}")
         print(s.BACK_OR_QUIT)
         if s.ERROR:
             print(s.MSG_ERROR)
 
-    def display_no_subsitute(self):
-        """Display the substitute."""
-        print(
-            "\nIl n'y à pas de produit avec un meilleur\
- nutriscore dans cette catégorie.\n"
-        )
-        print(s.SUB_HEADER)
+    def display_favorites(self, substituted, substitute):
+        """Display the saved products."""
+        print(self.header())
+        print(f"\n{s.SAVED_PRODUCTS}\n")
+        print(f"\n{s.SUBSTITUTED_DETAIL} :\n")
+        for item in [substituted]:
+            print(
+                f"{s.PRODUCT} : {item.name}\
+\n{s.BRAND} : {item.brand.replace(',', ', ')}\
+\n{s.STORES} : {item.stores.replace(',', ', ')}\
+\n{s.URL} : {item.url}\
+\n{s.NUTRISCORE.capitalize()} : {item.nutriscore.capitalize()}"
+            )
+        print(f"\n{s.SUBSTITUTE_DETAIL} :\n")
+        for item in [substitute]:
+            print(
+                f"{s.PRODUCT} : {item.name}\
+\n{s.BRAND} : {item.brand.replace(',', ', ')}\
+\n{s.STORES} : {item.stores.replace(',', ', ')}\
+\n{s.URL} : {item.url}\
+\n{s.NUTRISCORE.capitalize()} : {item.nutriscore.capitalize()}"
+            )
+        print(f"{s.CR}{s.SUB_HEADER}")
         print(s.BACK_OR_QUIT)
         if s.ERROR:
             print(s.MSG_ERROR)
 
-    def display_no_subsitute_confirmed(self, instance):
-        """Display the substitute."""
-        print("\nDétail du produit sélectionné :\n")
+    def display_no_subsitute(self, product):
+        """Display an alert message.
+
+        If there is no substitute because nutriscore is worse than A.
+        """
+        print(self.header())
         print(
-            f"Produit : {instance.name}\nMarque : {instance.brand}\nMagasin :\
- {instance.stores}\nURL : {instance.url}\nNutriscore :\
- {instance.nutriscore.capitalize()}"
+            f"\n{s.YOU_HAVE_SELECTED}{product.name},{s.NUTRISCORE}\
+ {product.nutriscore.capitalize()}"
         )
-        print("\nCe produit à le meilleur nutriscore dans sa catégorie.\n")
-        print(s.SUB_HEADER)
+        print(f"\n{s.NO_SUBSTITUTE}\n")
+        print(f"{s.CR}{s.SUB_HEADER}")
         print(s.BACK_OR_QUIT)
         if s.ERROR:
             print(s.MSG_ERROR)
 
-    def display_no_better_nutriscore(self):
-        """Display the substitute."""
-        print("\nVotre choix est très bon, le nutriscore est le meilleur possible.\n")
-        print(s.SUB_HEADER)
+    def display_no_better_nutriscore(self, product):
+        """Display an alert message if no substitute since the nutriscore is A."""
+        print(self.header())
+        print(
+            f"\n{s.YOU_HAVE_SELECTED} : {product.name},{s.NUTRISCORE}\
+ {product.nutriscore.capitalize()}"
+        )
+        print(f"\n{s.BEST_CHOICE}\n")
+        print(f"{s.CR}{s.SUB_HEADER}")
         print(s.BACK_OR_QUIT)
         if s.ERROR:
             print(s.MSG_ERROR)
 
     def display_save_substitute(self):
-        """Display the substitute."""
-        print("\nLe produit à bien été enregistré dans votre liste.\n")
-        print(s.SUB_HEADER)
+        """Display confirmation of saving substitute."""
+        print(self.header())
+        print(f"\n{s.CONFIRM_TITLE}\n")
+        print(f"\n{s.CONFIRMATION}\n")
+        print(f"{s.CR}{s.SUB_HEADER}")
         print(s.BACK_OR_QUIT)
         if s.ERROR:
             print(s.MSG_ERROR)
 
-    def display_saved_products(self, substitutes):
-        """Display the saved products as substitutes."""
-        print("\nLes substituts que vous avez sélectionné :\n")
+    def display_saved_products(self):
+        """Display products saved as substitutes."""
+        print(self.header())
+        print(f"\n{s.SAVED_PRODUCTS}\n")
 
-    def input_message(self, list_chosen):
-        """Input the message."""
+    def input_message_menu(self):
+        """Input the menu message."""
+        print()
         choice = "\n".join(
             [
-                colored(f"{nb}", "yellow") + f". {item.name} "
-                for nb, item in enumerate(list_chosen, 1)
+                colored(str(index), "yellow") + ". " + item
+                for index, item in s.MENU_CHOICES.items()
             ]
         )
         print(choice)
+        print(f"{s.CR}{s.SUB_HEADER}")
+        if s.ERROR:
+            print(s.MSG_ERROR)
+        print(s.BACK_OR_QUIT)
 
-    def input_message_categories(self):
-        """Input the message."""
+    def input_categories(self, categories):
+        """Input the categories."""
         choice = "\n".join(
             [
-                colored(f"{nb}", "yellow") + f". {item.name} "
-                for nb, item in enumerate(self.categories, 1)
+                colored(self.justify(str(index), 1), "yellow") + ". " + item.name
+                for index, item in enumerate(categories, 1)
             ]
         )
         print(choice)
+        print(f"{s.CR}{s.SUB_HEADER}")
+        if s.ERROR:
+            print(s.MSG_ERROR)
+        print(s.BACK_OR_QUIT)
 
-    def input_message_products(self):
-        """Input the message."""
+    def input_products(self, products):
+        """Input the products."""
         choice = "\n".join(
             [
-                colored(f"{nb}", "yellow") + f". {item.name} "
-                for nb, item in enumerate(self.products, 1)
+                colored(self.justify(str(index), 1), "yellow") + ". " + item.name
+                for index, item in enumerate(products, 1)
             ]
         )
         print(choice)
+        print(f"{s.CR}{s.SUB_HEADER}")
+        if s.ERROR:
+            print(s.MSG_ERROR)
+        print(s.BACK_OR_QUIT)
 
     def input_save(self):
-        """Input the message."""
+        """Input the save message."""
         choice = "\n".join(
             [
-                colored(f"{nb}", "yellow") + f". {item} "
-                for nb, item in s.MSG_SAVE.items()
+                colored(str(index), "yellow") + ". " + item
+                for index, item in s.MSG_SAVE.items()
             ]
         )
         print(choice)
 
     def input_saved(self):
-        """Input the message."""
+        """Input the saved message."""
         choice = "\n".join(
             [
-                colored(f"{nb}", "yellow") + f". {item} "
-                for nb, item in s.MSG_SAVED.items()
+                colored(str(index), "yellow") + ". " + item
+                for index, item in s.MSG_SAVED.items()
             ]
         )
         print(choice)
 
     def input_saved_products(self, substitutes):
-        """Input the message."""
+        """Input the saved products ."""
         choice = "\n".join(
             [
-                colored(f"{nb}", "yellow") + f". {item}"
-                for nb, item in enumerate(substitutes, 1)
+                colored(self.justify(str(index), 1), "yellow") + ". " + item.name
+                for index, item in enumerate(substitutes, 1)
             ]
         )
         print(choice)
+        print(f"{s.CR}{s.SUB_HEADER}")
+        if s.ERROR:
+            print(s.MSG_ERROR)
+        print(s.BACK_OR_QUIT)
 
-    def input_best_product(self):
-        """Input the message."""
+    def input_menu_best_product(self):
+        """Input the saved products details."""
         choice = "\n".join(
             [
-                colored(f"{nb}", "yellow") + f". {item} "
-                for nb, item in s.MENU_CHOICES.items()
+                colored(index, "yellow") + ". " + item
+                for index, item in s.MENU_CHOICES.items()
             ]
         )
         print(choice)
